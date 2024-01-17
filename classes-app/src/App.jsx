61dc6d577f87cab576da7543443053fc43c7a4d6
HelgaUa <Elgaua@gmail.com>
2024-01-17 15:42:08 +0200

    HW28 corrected

--- classes-app/src/App.jsx
+++ classes-app/src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
         }
     };
 
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate() {
         const isWinner = calculateWinner(this.state.squares);
         console.log(isWinner);
         if (isWinner) {
